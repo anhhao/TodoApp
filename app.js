5c719d8fd797dbf3665dcfafd5c8a33013162dae
Anh Nguyen <anhhao1102@gmail.com>
2015-11-11 10:40:05 -0700

    5

--- app.js
+++ app.js
@@ -1,60 +1,88 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// configuration =================
 
-var app = express();
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// define model =================
+var Todo = mongoose.model('Todo', {
+  text : String
 });
 
-// error handlers
+// routes ======================================================================
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+// api ---------------------------------------------------------------------
+// get all todos
+app.get('/api/todos', function(req, res) {
+
+  // use mongoose to get all todos in the database
+  Todo.find(function(err, todos) {
+
+    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+    if (err)
+      res.send(err)
+
+    res.json(todos); // return all todos in JSON format
+  });
+});
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+  // create a todo, information comes from AJAX request from Angular
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+    // get and return all the todos after you create another
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
     });
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+    // get and return all the todos after you create another
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
   });
 });
 
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
-module.exports = app;
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
